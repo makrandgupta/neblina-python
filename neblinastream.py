82e34248437d716ebda6971e7d250a15d0e70f55
Alexandre Courtemanche <a.courtemanche@motsai.com>
2015-11-13 11:57:08 -0500

    Made a second window for showing heading values

--- neblinastream.py
+++ neblinastream.py
@@ -1,5 +1,6 @@
 import pyqtgraph as pg
-from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
 import numpy as np
 import neblina as neb
 import neblinasim as sim
@@ -9,13 +10,15 @@ from pyqtgraph.parametertree import Parameter, ParameterTree, ParameterItem, reg
 import sys
 import time
 
-class DataThread(QtCore.QThread):
+class DataThread(QThread):
     """docstring for DataThread"""
-    def __init__(self, data, updateSignal, parent = None):
-        QtCore.QThread.__init__(self, parent)
+    def __init__(self, data, IMUupdateSignal,
+                headingWindow, parent = None):
+        QThread.__init__(self, parent)
         self.exiting = False
         self.data = data
-        self.updateSignal = updateSignal
+        self.IMUupdateSignal = IMUupdateSignal
+        self.headingWindow = headingWindow
 
     def __del__(self):
         self.exiting = True
@@ -24,56 +27,161 @@ class DataThread(QtCore.QThread):
     def run(self):
         while (self.exiting == False):
             self.data.update()
-            self.updateSignal.emit()
+            self.IMUupdateSignal.emit()
+            self.headingWindow.setAngle(self.data.headingData)
             time.sleep(0.02)
 
 class PlottingData(object):
     """docstring Por PlottingData"""
     def __init__(self, arg):
-        self.rollIdx = 0
-        self.numSamples = 1000
+        self.samplingFrequency = 50.0
+        self.imuRollIdx = 0
+        self.headingRollIdx = 0
+        self.numIMUSamples = 1000
         graphSize = 250
         self.timebase = np.arange(0, graphSize)
         self.accelData = [[],[],[]]
         self.gyroData = [[],[],[]]
-        self.imuPackets = sim.createRandomIMUDataPacketList(50.0, self.numSamples, 0.5)
+        self.headingData = 0.0
+        self.imuPackets = sim.createRandomIMUDataPacketList(
+            self.samplingFrequency, self.numIMUSamples, 0.5)
+        self.eulerAnglePackets = sim.createSpinningObjectPacketList(
+            self.samplingFrequency, yawRPS=0.25)
+
+        # Populate the initial IMU data list
         for elem in (packet for idx,packet in enumerate(self.imuPackets) if idx < graphSize):
             # For all three axis
             for idx,axisSample in enumerate(elem.data.accel):
                 # Gyro and Accel have same num of axis
                 self.accelData[idx].append(axisSample)
                 self.gyroData[idx].append(axisSample)
+        # Populate the initial euler heading data
+        self.headingData = self.eulerAnglePackets[0].data.yaw
 
     def update(self):
         # Update all three axis for both accel and gyro
         for idx,axis in enumerate(self.accelData):
             self.accelData[idx] = list(np.roll(self.accelData[idx], 1))
-            self.accelData[idx][0] = self.imuPackets[self.rollIdx].data.accel[idx]
+            self.accelData[idx][0] = self.imuPackets[self.imuRollIdx].data.accel[idx]
         for idx,axis in enumerate(self.gyroData):
             self.gyroData[idx] = list(np.roll(self.gyroData[idx], 1))
-            self.gyroData[idx][0] = self.imuPackets[self.rollIdx].data.gyro[idx]
+            self.gyroData[idx][0] = self.imuPackets[self.imuRollIdx].data.gyro[idx]
+        # Update euler angle heading
+        self.headingData = self.eulerAnglePackets[self.headingRollIdx].data.yaw
+
         # Wrap around the samples
-        self.rollIdx += 1
-        self.rollIdx %= (self.numSamples)
+        self.imuRollIdx += 1    
+        self.imuRollIdx %= (self.numIMUSamples)
+        self.headingRollIdx += 1
+        self.headingRollIdx %= len(self.eulerAnglePackets)
+
+# This window inspired by: https://wiki.python.org/moin/PyQt/Compass%20widget
+class HeadingWindow(QWidget):
+    angleChangedSignal = pyqtSignal(float)
+    def __init__(self, parent=None):
+        super(HeadingWindow, self).__init__(parent)
+        self._angle = 0.0
+        self._margins = 10
+        self._pointText = {0: "N", 45: "NE", 90: "E",
+                             135: "SE", 180: "S",
+                           225: "SW", 270: "W", 315: "NW"}
+     
+    def paintEvent(self, event):
+        # print('redraw')
+        painter = QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QPainter.Antialiasing)         
+        painter.fillRect(event.rect(), self.palette().brush(QPalette.Window))
+        self.drawMarkings(painter)
+        self.drawNeedle(painter)
+        painter.end()
+
+    def drawMarkings(self, painter):
+    
+        painter.save()
+        painter.translate(self.width()/2, self.height()/2)
+        scale = min((self.width() - self._margins)/120.0,
+                    (self.height() - self._margins)/120.0)
+        painter.scale(scale, scale)
         
+        font = QFont(self.font())
+        font.setPixelSize(10)
+        metrics = QFontMetricsF(font)
+        
+        painter.setFont(font)
+        painter.setPen(self.palette().color(QPalette.Shadow))
+        
+        i = 0
+        while i < 360:
+            if i % 45 == 0:
+                painter.drawLine(0, -40, 0, -50)
+                painter.drawText(-metrics.width(self._pointText[i])/2.0, -52,
+                                 self._pointText[i])
+            else:
+                painter.drawLine(0, -45, 0, -50)
+            painter.rotate(15)
+            i += 15
+        
+        painter.restore()
+    
+    def drawNeedle(self, painter):
+    
+        painter.save()
+        painter.translate(self.width()/2, self.height()/2)
+        painter.rotate(self._angle)
+        scale = min((self.width() - self._margins)/120.0,
+                    (self.height() - self._margins)/120.0)
+        painter.scale(scale, scale)
+        
+        painter.setPen(QPen(0))
+        painter.setBrush(self.palette().brush(QPalette.Shadow))
+        
+        painter.drawPolygon(
+            QPolygon([QPoint(-10, 0), QPoint(0, -45), QPoint(10, 0),
+                      QPoint(0, 45), QPoint(-10, 0)])
+            )
+        
+        painter.setBrush(self.palette().brush(QPalette.Highlight))
+        
+        painter.drawPolygon(
+            QPolygon([QPoint(-5, -25), QPoint(0, -45), QPoint(5, -25),
+                      QPoint(0, -30), QPoint(-5, -25)])
+            )
+        
+        painter.restore()
+    
+    def sizeHint(self):    
+        return QSize(1000, 750)
+    
+    def angle(self):
+        return self._angle
+
+    @pyqtSlot(float)
+    def setAngle(self, angle):
+        if angle != self._angle:
+            self._angle = angle
+            self.angleChangedSignal.emit(angle)
+            self.update()
+    
+    angle = pyqtProperty(float, angle, setAngle)
+
+
+
 
 class PlottingWindow(pg.GraphicsWindow):
     """docstring for PlottingGUI"""
-    packetReceivedSignal = QtCore.Signal()
+    packetReceivedSignal = Signal()
     def __init__(self, plottingData, parent=None):
         super(PlottingWindow, self).__init__(parent)
         self.packetReceivedSignal.connect(self.draw)
-        self.showMaximized()
         self.data = plottingData
-        self.setWindowTitle('Neblina Data Plots')
+        self.setWindowTitle('Neblina Accel/Gyro')
         self.accelPlot = self.addPlot(title='Accelerometer Data')
         self.accelPlot.addLegend()
         self.nextRow()
         self.gyroPlot = self.addPlot(title='Gyroscope Data')
         self.gyroPlot.addLegend()
         self.nextRow()
-        # self.magPlot = self.addPlot(title='Magnetometer Data')
-        # self.magPlot.addLegend()
         self.accelXCurve = self.accelPlot.plot(self.data.timebase, self.data.accelData[0], pen='b', name='X')
         self.accelYCurve = self.accelPlot.plot(self.data.timebase, self.data.accelData[1], pen='g', name='Y')
         self.accelZCurve = self.accelPlot.plot(self.data.timebase, self.data.accelData[2], pen='r', name='Z')
@@ -97,28 +205,25 @@ def start():
     # of the GUI...)
 
     data = PlottingData(1)
-    plottingApplication = QtGui.QApplication(sys.argv) # create application
-    plottingWindow = PlottingWindow(data) # Create the instance of the window
+    plottingApplication = QApplication(sys.argv) # create application
+    plottingWindow = PlottingWindow(data) # Create the instance of the plotting window
+    headingWindow = HeadingWindow() # Create the instance of the heading window
 
-    dataWorkerThread = DataThread(data, plottingWindow.packetReceivedSignal)
+    dataWorkerThread = DataThread(data, plottingWindow.packetReceivedSignal, headingWindow)
     # drawThread = DrawingThread(plottingWindow)
 
     # Show windows
     plottingWindow.show() # Make the instance visible
     plottingWindow.raise_() # Raise instance on top of window stack
+    plottingWindow.resize(500,500)
+    headingWindow.show() # Make the instance visible
+    headingWindow.raise_() # Raise instance on top of window stack
+    headingWindow.move(500,0)
     
     # Start the threads
     # drawThread.start()
     dataWorkerThread.start()
     plottingApplication.exec_()
-
-    # timer = pg.QtCore.QTimer()      # timer used to call the updateGraphs function
-    # timer.timebaseout.connect(updateGraphs)
-    # timer.start(500)                  # timer restarts every 5ms ( freq = 200 Hz)
-
-    # if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-    #     QtGui.QApplication.instance().exec_()
-    # ser.close()
 
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
