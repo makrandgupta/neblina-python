cf83e2b0e7d7eecc62bd77a1a4f02942834df719
Jean-Samuel Chenard <js.chenard@motsai.com>
2015-11-15 07:05:03 -0500

    standing sitting

--- neblina3d.py
+++ neblina3d.py
@@ -14,30 +14,31 @@ def main():
     else:
         ble = nebStream.BLENeblinaStream()
 
-    scene = display(title='Neblina3D Demo',
-     x=200, y=200, width=1000, height=800, scale=(0.21,0.21,0.21),
-     center=(0,-1,-1), forward=(0,1,0), background=(0,1,1))
-    staticframe = frame()
-    staticframe.axis = (1,0,0)
-    f = frame()
-    f.axis = (1,0,0)
+    #scene = display(title='Neblina3D Demo',
+    # x=200, y=200, width=1000, height=800, scale=(0.21,0.21,0.21),
+    # center=(0,-1,-1), forward=(0,1,0), background=(0,1,1))
+    #staticframe = frame()
+    #staticframe.axis = (1,0,0)
+    #f = frame()
+    #f.axis = (1,0,0)
     
     # Create 3D models
-    cylinder(frame=f, pos=(0,0,0), radius=0.25, length=2,color=color.cyan) # body
-    box(frame=f, pos=(1,1,0), length=0.5, width=0.2, height=2, color=color.red)
-    box(frame=f, pos=(1,-1,0), length=0.5, width=0.2, height=2, color=color.red)
-    box(frame=f, pos=(0,0.25,0), length=0.2, width=0.2, height=0.35, color=color.yellow) # tail wings
-    box(frame=f, pos=(0,-0.25,0), length=0.2, width=0.2, height=0.35, color=color.yellow)
-    box(frame=f, pos=(0.25 ,0,-0.5), length=0.4, width=0.75, height=0.1, color=color.yellow) # rudder
-    pyramid(frame = f, pos=(2,0,0), size=(2,1,0.5)) # cockpit
+    #cylinder(frame=f, pos=(0,0,0), radius=0.25, length=2,color=color.cyan) # body
+    #box(frame=f, pos=(1,1,0), length=0.5, width=0.2, height=2, color=color.red)
+    #box(frame=f, pos=(1,-1,0), length=0.5, width=0.2, height=2, color=color.red)
+    #box(frame=f, pos=(0,0.25,0), length=0.2, width=0.2, height=0.35, color=color.yellow) # tail wings
+    #box(frame=f, pos=(0,-0.25,0), length=0.2, width=0.2, height=0.35, color=color.yellow)
+    #box(frame=f, pos=(0.25 ,0,-0.5), length=0.4, width=0.75, height=0.1, color=color.yellow) # rudder
+    #pyramid(frame = f, pos=(2,0,0), size=(2,1,0.5)) # cockpit
 
     # Origin arrows
-    arrow(frame=staticframe, pos=(0,0,0), axis=(1.5,0,0), shaftwidth=0.05, color=color.red) # yaw
-    arrow(frame=staticframe, pos=(0,0,0), axis=(0,1.5,0), shaftwidth=0.05, color=color.green) # pitch
-    arrow(frame=staticframe, pos=(0,0,0), axis=(0,0,1.5), shaftwidth=0.05, color=color.blue) # roll
+    #arrow(frame=staticframe, pos=(0,0,0), axis=(1.5,0,0), shaftwidth=0.05, color=color.red) # yaw
+    #arrow(frame=staticframe, pos=(0,0,0), axis=(0,1.5,0), shaftwidth=0.05, color=color.green) # pitch
+    #arrow(frame=staticframe, pos=(0,0,0), axis=(0,0,1.5), shaftwidth=0.05, color=color.blue) # roll
 
     ii = 0
     angles = [0]*3
+    position = "standing"	
     while True:
         if simulatedData == True:
             angles[0] = packetList[ii].data.yaw
@@ -45,8 +46,16 @@ def main():
             angles[2] = packetList[ii].data.roll
         else:
             angles = ble.getEulerAngles()
- 	
-	print angles
+            if (angles[2] < -45):
+                if (position == "sitting"):
+                    position = "standing"
+                    print angles 
+                    print "======== STANDING 1111111111111111111111111111111111111111111"
+            else:
+                if (position == "standing"):
+                    position = "sitting"
+                    print angles 
+                    print "======== SITTING ---------------------------------------------"
        
 #        f2 = frame()
 #        f2.axis = (1,0,0) # change orientation of both objects
