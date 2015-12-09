31b8a887c67c300e020c02fce9e209d4beeaa03a
Alexandre Courtemanche <a.courtemanche@motsai.com>
2015-12-09 13:43:39 -0500

    Fixes #4

---
+++ streammenu.py
@@ -0,0 +1,107 @@
+# Neblina streaming data utility
+# (C) 2015 Motsai Research Inc.
+# Author: Alexandre Courtemanche (a.courtemanche@motsai.com)
+
+import os
+import cmd
+import readline
+import slip
+import neblina as neb
+import neblinastream as streamingGUI
+
+class StreamMenu(cmd.Cmd):
+    """docstring for StreamMenu"""
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = '>>'
+        self.intro = "Welcome to the Neblina Streaming Menu!"
+
+    ## Command definitions ##
+    def do_hist(self, args):
+        """Print a list of commands that have been entered"""
+        print(self._hist)
+
+    def do_exit(self, args):
+        """Exits from the console"""
+        return -1
+
+    ## Command definitions to support Cmd object functionality ##
+    def do_EOF(self, args):
+        """Exit on system end of file character"""
+        return self.do_exit(args)
+
+    def do_shell(self, args):
+        """Pass command to a system shell when line begins with '!'"""
+        os.system(args)
+
+    def do_help(self, args):
+        """Get help on commands
+           'help' or '?' with no arguments prints a list of commands for which help is available
+           'help <command>' or '? <command>' gives help on <command>
+        """
+        ## The only reason to define this method is for the help text in the doc string
+        cmd.Cmd.do_help(self, args)
+
+    def do_streamEulerAngles(self, args):
+        if (len(args) == 0):
+            print('Need to specify a COM port (Example: /dev/ttyACM0 or COM3)')
+            return
+        myslip = slip.slip()
+        consoleStream = open(args, 'rb')
+        while(True):
+            consoleBytes = myslip.receivePacketFromStream(consoleStream)
+            nebPacket = neb.NebResponsePacket(consoleBytes)
+            print('yaw, pitch, roll: {0},{1},{2}'.format(nebPacket.data.yaw, 
+                nebPacket.data.pitch, nebPacket.data.roll))
+        # print(type(consoleBytes))
+        consoleStream.close()
+
+
+    ## Override methods in Cmd object ##
+    def preloop(self):
+        """Initialization before prompting user for commands.
+           Despite the claims in the Cmd documentaion, Cmd.preloop() is not a stub.
+        """
+        cmd.Cmd.preloop(self)   ## sets up command completion
+        self._hist    = []      ## No history yet
+        self._locals  = {}      ## Initialize execution namespace for user
+        self._globals = {}
+
+    def postloop(self):
+        """Take care of any unfinished business.
+           Despite the claims in the Cmd documentaion, Cmd.postloop() is not a stub.
+        """
+        cmd.Cmd.postloop(self)   ## Clean up command completion
+        print ("Exiting...")
+
+    def precmd(self, line):
+        """ This method is called after the line has been input but before
+            it has been interpreted. If you want to modify the input line
+            before execution (for example, variable substitution) do it here.
+        """
+        self._hist += [ line.strip() ]
+        return line
+
+    def postcmd(self, stop, line):
+        """If you want to stop the console, return something that evaluates to true.
+           If you want to do some post command processing, do it here.
+        """
+        return stop
+
+    def emptyline(self):    
+        """Do nothing on empty input line"""
+        pass
+
+    def default(self, line):       
+        """Called on an input line when the command prefix is not recognized.
+           In that case we execute the line as Python code.
+        """
+        try:
+            exec(line) in self._locals, self._globals
+        except Exception as e:
+            print (e.__class__, ":", e)
+
+
+if __name__ == '__main__':
+    console = StreamMenu()
+    console . cmdloop() 
