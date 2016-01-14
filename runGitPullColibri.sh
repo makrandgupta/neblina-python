6695828ebd3a8e31df8576ca7f90b4f30d53031a
emmanhello <e.lambert@motsai.com>
2016-01-14 18:05:07 -0500

    Removed some hardcoded path
    -runTests script will now created a temp folder named tempHexfolder and
    will use the hex in that folder to program RF and MCU.
    -git will pull colibri's latest code from master instead of latest
    stable version

--- runGitPullColibri.sh
+++ runGitPullColibri.sh
@@ -2,6 +2,9 @@ cd /c/Users/Manou/Documents/GitHub/colibri
 eval $(ssh-agent -s)
 ssh-add /c/Users/Manou/.ssh/github_rsa 
 git fetch -a
-git checkout v0.11
+git checkout master
+git pull
+crash
 sleep 3
+
 
--- runTests.bat
+++ runTests.bat
@@ -2,6 +2,7 @@
 @echo off
 echo hi! this will test the neblina using test jig and a latero. 
 echo Make sure the latero is connected to same network and it is connected to the test jig. 
+if not exist %CD%\tempHexFolder (md %CD%\tempHexFolder)
 
 
 :usrChoice
@@ -13,7 +14,7 @@ echo 2. Execute Voltage test
 echo 3. Program freescale MCU
 echo 4. Program nordic RF
 echo 5. Stream Test
-set /p answer=e. Exit: 
+set /p answer=e. clean exit: 
 if "%answer%"=="e" (GOTO iQuit)
 if "%answer%"=="0" (GOTO Compile)
 if "%answer%"=="1" (GOTO All)
@@ -28,13 +29,16 @@ GOTO usrChoice
 set alltest=1
 GOTO vTest
 
-:Compile
-::must add C:\Program Files (x86)\Git\bin to Environment Variables: PATH
-"C:\Program Files\Git\git-bash.exe" runGitPullColibri.sh || ( echo git FAILED & GOTO usrChoice )
+:Compile ::these path are hard coded, would need to git pull into a temp folder and program hex.
+"C:\Program Files\Git\git-bash.exe" runGitPullColibri.sh || ( echo Please install git bash & GOTO usrChoice )
 
-"C:\eclipse\eclipsec.exe" --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data C:\Users\Manou\workspace.kinetis -cleanBuild CMSIS/Debug || echo compile CMSIS FAILED & GOTO usrChoice
-"C:\eclipse\eclipsec.exe" --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data C:\Users\Manou\workspace.kinetis -cleanBuild colibri/Debug || echo compile colibri FAILED & GOTO usrChoice
-"C:\eclipse\eclipsec.exe" --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data C:\Users\Manou\workspace.kinetis -cleanBuild NebKL26/Debug || echo compile NebKL26 FAILED & GOTO usrChoice
+"C:\eclipse\eclipsec.exe" --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data C:\Users\Manou\workspace.kinetis -cleanBuild CMSIS/Debug || (echo compile CMSIS FAILED & GOTO usrChoice)
+"C:\eclipse\eclipsec.exe" --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data C:\Users\Manou\workspace.kinetis -cleanBuild colibri/Debug || (echo compile colibri FAILED & GOTO usrChoice)
+"C:\eclipse\eclipsec.exe" --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data C:\Users\Manou\workspace.kinetis -cleanBuild NebKL26/Debug || (echo compile NebKL26 FAILED & GOTO usrChoice)
+copy "C:\Users\Manou\Dropbox\Neblina_Production\Firmware\KL26Z\kinetis.cfg" %CD%\tempHexFolder
+copy "C:\Users\Manou\Documents\GitHub\colibri\Freescale\kl26z\apps\NebKL26\Debug\NebKL26.hex" %CD%\tempHexFolder
+copy "C:\Users\Manou\Dropbox\Neblina_Production\Firmware\nRF51\s130_nrf51.hex" %CD%\tempHexFolder
+copy "C:\Users\Manou\Dropbox\Neblina_Production\Firmware\nRF51\NebnRF51_v0_10.hex" %CD%\tempHexFolder\NebnRF51.hex
 GOTO usrChoice
 
 :vTest
@@ -44,13 +48,17 @@ if %alltest%==0 (GOTO usrChoice)
 
 :ProgramFreescale
 set /p dummy=Please connect the programmer to the Freescale MCU's jtag
-"C:\Program Files (x86)\GNU ARM Eclipse\openocd\bin\openocd.exe" -f "C:\Users\Manou\Dropbox\Neblina_Production\Firmware\KL26Z\kinetis.cfg" -c "program C:/Users/Manou/Documents/GitHub/colibri/Freescale/kl26z/apps/NebKL26/Debug/NebKL26.hex verify  exit" || echo programming FAILED
+cd %CD%\tempHexFolder
+"C:\Program Files (x86)\GNU ARM Eclipse\openocd\bin\openocd.exe" -f "kinetis.cfg" -c "program NebKL26.hex verify exit" || echo programming FAILED
+cd ..
 if %alltest%==0 (GOTO usrChoice)
 
 :ProgramRF
 set /p dummy=Please connect the programmer to the Nordic RF's jtag
-"C:\Program Files (x86)\GNU ARM Eclipse\openocd\bin\openocd.exe" -f interface/cmsis-dap.cfg -f target/nrf51.cfg -c "program C:/Users/Manou/Dropbox/Neblina_Production/Firmware/nRF51/s130_nrf51.hex exit" || echo programming FAILED & GOTO usrChoice
-"C:\Program Files (x86)\GNU ARM Eclipse\openocd\bin\openocd.exe" -f interface/cmsis-dap.cfg -f target/nrf51.cfg -c "program C:/Users/Manou/Dropbox/Neblina_Production/Firmware/nRF51/NebnRF51_v0_10.hex exit" || echo programming FAILED
+cd %CD%\tempHexFolder
+"C:\Program Files (x86)\GNU ARM Eclipse\openocd\bin\openocd.exe" -f interface/cmsis-dap.cfg -f target/nrf51.cfg -c "program s130_nrf51.hex exit" || (echo programming FAILED & GOTO usrChoice)
+"C:\Program Files (x86)\GNU ARM Eclipse\openocd\bin\openocd.exe" -f interface/cmsis-dap.cfg -f target/nrf51.cfg -c "program NebnRF51.hex exit" || echo programming FAILED
+cd ..
 if %alltest%==0 (GOTO usrChoice)
 
 :StreamTest
@@ -60,5 +68,6 @@ GOTO usrChoice
 :iQuit
 taskkill /im ssh-agent.exe
 if EXIST streamconfig.txt (del streamconfig.txt)
+if EXIST  tempHexFolder (rd /S tempHexFolder)
 exit
 
