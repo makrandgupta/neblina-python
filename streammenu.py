c3b00b04f47da3df5eef434822a328b9650802b4
Alexandre Courtemanche <a.courtemanche@motsai.com>
2016-01-19 14:46:23 -0500

    Baud rate reset to 115200 to accomodate new firmware version

--- streammenu.py
+++ streammenu.py
@@ -39,7 +39,7 @@ class StreamMenu(cmd.Cmd):
         sc = None
         while sc is None:
             try:
-                sc = serial.Serial(port=comPortName,baudrate=230400)
+                sc = serial.Serial(port=comPortName,baudrate=115200)
             except serial.serialutil.SerialException as se:
                 if 'Device or resource busy:' in se.__str__():
                     print('Opening COM port is taking a little while, please stand by...')
