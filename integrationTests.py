9a551e0b876e60013a66c0772c05fa3cbdbf5e92
osarbishei <o.sarbishei@motsai.com>
2016-02-08 14:06:29 -0500

    baudrate changed to 500 Kbps for integration tests

--- integrationTests.py
+++ integrationTests.py
@@ -62,7 +62,7 @@ class ut_IntegrationTests(unittest.TestCase):
         sc = None
         while sc is None:
             try:
-                sc = serial.Serial(port=comPortName,baudrate=115200)
+                sc = serial.Serial(port=comPortName,baudrate=500000)
             except serial.serialutil.SerialException as se:
                 if 'Device or resource busy:' in se.__str__():
                     print('Opening COM port is taking a little while, please stand by...')
